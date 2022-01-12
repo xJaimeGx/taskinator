050a5f755bc8e559df6d4b3fefb0cb21a862276c
xJaimeGx <beanieman019@protonmail.com>
2022-01-11 19:16:32 -0600

    add form values

--- assets/js/scrpt.js
+++ assets/js/scrpt.js
@@ -4,12 +4,26 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
 
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    // give div a class name
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span";
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl);
+    
 };
 
 formEl.addEventListener("submit", createTaskHandler);
